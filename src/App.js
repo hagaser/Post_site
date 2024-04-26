9044ac6f7a55a2518d1c0e29f84509da2b55c6bd
hazrys <hagasers1550@gmail.com>
2024-04-26 18:01:20 +0300

    Input

--- src/App.js
+++ src/App.js
@@ -2,19 +2,33 @@ import React, {useState} from "react";
 
 function App() {
 
-  const [count, Change] = useState(0)
+  const [count, Change] = useState(0);
+  const [input_value, Change_Value] = useState('Текст');
 
   const Increase = () =>{
-    Change(count + 1)
+    Change(count + 1);
   }
 
   const Decrease = () =>{
-    Change(count - 1)
+    Change(count - 1);
+  }
+
+  const NumberCheck = (value) =>{
+    if (!isNaN(value) && (value)) {
+      return 0
+    }
+    else return value
   }
 
   return (
     <div className="App">
       <h1>{count}</h1>
+      <h1>{input_value}</h1>
+      <input
+        type="text"
+        value={input_value}
+        onChange={event => {Change_Value(NumberCheck(event.target.value))}}
+      />
       <button onClick={Increase}>Увеличить</button>
       <button onClick={Decrease}>Уменьшить</button>
     </div>
