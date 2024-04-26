4e8cd65f28907ee0c68ab272792fb5d110e2b378
hazrys <hagasers1550@gmail.com>
2024-04-26 18:25:26 +0300

    Components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React, {useState} from "react";
+import Counter from "./components/Counter";
+import ShowInput from "./components/ShowInput";
 
 function App() {
 
-  const [count, Change] = useState(0);
-  const [input_value, Change_Value] = useState('Текст');
-
-  const Increase = () =>{
-    Change(count + 1);
-  }
-
-  const Decrease = () =>{
-    Change(count - 1);
-  }
-
-  const NumberCheck = (value) =>{
-    if (!isNaN(value) && (value)) {
-      return 0
-    }
-    else return value
-  }
-
   return (
     <div className="App">
-      <h1>{count}</h1>
-      <h1>{input_value}</h1>
-      <input
-        type="text"
-        value={input_value}
-        onChange={event => {Change_Value(NumberCheck(event.target.value))}}
-      />
-      <button onClick={Increase}>Увеличить</button>
-      <button onClick={Decrease}>Уменьшить</button>
+      <Counter/>
+      <ShowInput/>
     </div>
   );
 }
---
+++ src/components/ShowInput.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+const ShowInput = () => {
+
+  const [input_value, Change_Value] = useState('Текст');
+
+  const NumberCheck = (value) =>{
+    if (!isNaN(value) && (value)) {
+      return 0;
+    }
+      else return value;
+    }
+
+  return (
+    <div>
+      <h1>{input_value}</h1>
+      <input
+        type="text"
+        value={input_value}
+        onChange={event => {Change_Value(NumberCheck(event.target.value))}}
+      />
+    </div>
+  );
+};
+
+export default ShowInput;
