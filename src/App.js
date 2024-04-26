80863f527d1f95ca01551860372b2ed00bb4371c
hazrys <hagasers1550@gmail.com>
2024-04-26 17:10:11 +0300

    Counter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 
 function App() {
+
+  const [count, Change] = useState(0)
+
+  const Increase = () =>{
+    Change(count + 1)
+  }
+
+  const Decrease = () =>{
+    Change(count - 1)
+  }
+
   return (
     <div className="App">
+      <h1>{count}</h1>
+      <button onClick={Increase}>Увеличить</button>
+      <button onClick={Decrease}>Уменьшить</button>
     </div>
   );
 }
